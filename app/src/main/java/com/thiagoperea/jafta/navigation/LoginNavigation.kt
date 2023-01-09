b63ad7f507a646735d5fdee69517df5ca6b6133e
Thiago Perea <thiago.perea1@gmail.com>
2023-01-09 18:50:18 -0300

    Empty signup and login screens

--- app/src/main/java/com/thiagoperea/jafta/navigation/LoginNavigation.kt
+++ app/src/main/java/com/thiagoperea/jafta/navigation/LoginNavigation.kt
@@ -4,7 +4,9 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.thiagoperea.jafta.JaftaNavigation
+import com.thiagoperea.jafta.login.ui.login.LoginScreen
 import com.thiagoperea.jafta.login.ui.onboarding.OnboardingScreen
+import com.thiagoperea.jafta.login.ui.signup.SignupScreen
 
 fun NavGraphBuilder.loginNavigation(navController: NavController) {
 
@@ -13,5 +15,13 @@ fun NavGraphBuilder.loginNavigation(navController: NavController) {
             goToSignup = { navController.navigate(JaftaNavigation.signUp) },
             goToLogin = { navController.navigate(JaftaNavigation.login) }
         )
+    }
+
+    composable(JaftaNavigation.signUp) {
+        SignupScreen()
+    }
+
+    composable(JaftaNavigation.login) {
+        LoginScreen()
     }
 }
