29b653e3818e0dad4e9fe890b661e3076c0bd422
nevesRafa <rafaelneves02@gmail.com>
2023-01-10 14:53:18 -0300

    Criando tela LoginScreen

--- design_system/src/main/java/com/thiagoperea/jafta/design_system/theme/TextStyles.kt
+++ design_system/src/main/java/com/thiagoperea/jafta/design_system/theme/TextStyles.kt
@@ -19,8 +19,15 @@ object TextStyles {
     )
 
     val title3 = TextStyle(
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.W600,
         fontSize = 18.sp,
         //TODO: fontFamily =
+    )
+
+    val textField = TextStyle(
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+        lineHeight = 18.sp,
+        color = Light20
     )
 }
