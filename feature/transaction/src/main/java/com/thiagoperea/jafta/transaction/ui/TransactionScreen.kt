c412e1d806a2296323c8c8d69f2a8a569aa4f89f
Thiago Perea <thiago.perea1@gmail.com>
2023-01-28 17:45:04 -0300

    Partial commit to transaction flow

--- feature/transaction/src/main/java/com/thiagoperea/jafta/transaction/ui/TransactionScreen.kt
+++ feature/transaction/src/main/java/com/thiagoperea/jafta/transaction/ui/TransactionScreen.kt
@@ -1,17 +1,36 @@
 package com.thiagoperea.jafta.transaction.ui
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import com.thiagoperea.jafta.design_system.LoadingScreen
 import com.thiagoperea.jafta.design_system.dialog.DialogState
+import com.thiagoperea.jafta.design_system.theme.Dark100
+import com.thiagoperea.jafta.design_system.theme.Red100
+import com.thiagoperea.jafta.design_system.theme.TextStyles
+import com.thiagoperea.jafta.design_system.theme.Violet100
 import com.thiagoperea.jafta.transaction.ui.type.TransactionScreenType
 import com.thiagoperea.jafta.transaction.ui.view.TransactionRepeatSheet
 import kotlinx.coroutines.launch
@@ -52,12 +71,109 @@ fun TransactionScreen(
     }
 
     /* TODO: EXT FUN TO HANDLE DIALOGS */
-    when (viewModel.dialogState.value) {
+    val dialogState = viewModel.dialogState.value
+
+    when (dialogState) {
         is DialogState.Dismissed -> Unit
-        is DialogState.Failure -> TODO()
-        is DialogState.Loading -> TODO()
-        is DialogState.Success -> TODO()
+        is DialogState.Loading -> {
+            AlertDialog(
+                onDismissRequest = {},
+                shape = RoundedCornerShape(16.dp),
+                properties = DialogProperties(
+                    dismissOnBackPress = false,
+                    dismissOnClickOutside = false
+                ),
+                buttons = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(48.dp)
+                        )
+
+                        Spacer(Modifier.height(16.dp))
+
+                        Text(
+                            text = stringResource(dialogState.messageRes),
+                            style = TextStyles.body3,
+                            color = Dark100
+                        )
+                    }
+                }
+            )
+        }
+        is DialogState.Failure -> {
+            AlertDialog(
+                onDismissRequest = {},
+                shape = RoundedCornerShape(16.dp),
+                properties = DialogProperties(
+                    dismissOnBackPress = false,
+                    dismissOnClickOutside = false
+                ),
+                buttons = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(48.dp),
+                            painter = painterResource(com.thiagoperea.jafta.design_system.R.drawable.ic_warning),
+                            tint = Red100,
+                            contentDescription = null
+                        )
+
+                        Spacer(Modifier.height(16.dp))
+
+                        Text(
+                            text = stringResource(dialogState.messageRes),
+                            style = TextStyles.body3,
+                            color = Dark100
+                        )
+                    }
+                }
+            )
+        }
+        is DialogState.Success -> {
+            AlertDialog(
+                onDismissRequest = {},
+                shape = RoundedCornerShape(16.dp),
+                properties = DialogProperties(
+                    dismissOnBackPress = false,
+                    dismissOnClickOutside = false
+                ),
+                buttons = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(48.dp),
+                            painter = painterResource(com.thiagoperea.jafta.design_system.R.drawable.ic_success),
+                            tint = Violet100,
+                            contentDescription = null
+                        )
+
+                        Spacer(Modifier.height(16.dp))
+
+                        Text(
+                            text = stringResource(dialogState.messageRes),
+                            style = TextStyles.body3,
+                            color = Dark100
+                        )
+                    }
+                }
+            )
+        }
     }
+
+
 
     ModalBottomSheetLayout(
         sheetState = bottomSheetState,
--- feature/transaction/src/main/java/com/thiagoperea/jafta/transaction/ui/TransactionViewModel.kt
+++ feature/transaction/src/main/java/com/thiagoperea/jafta/transaction/ui/TransactionViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.thiagoperea.jafta.design_system.dialog.DialogState
 import com.thiagoperea.jafta.transaction.R
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class TransactionViewModel : ViewModel() {
@@ -23,6 +24,18 @@ class TransactionViewModel : ViewModel() {
             /* TODO: load data if it is editing */
 
             screenState.value = TransactionState.NewEntry
+
+            delay(2000L)
+            dialogState.value = DialogState.Loading(R.string.saving_transaction)
+
+            delay(3000L)
+            dialogState.value = DialogState.Success(R.string.transaction_saved)
+
+            delay(3000L)
+            dialogState.value = DialogState.Failure(R.string.saving_transaction_error)
+
+            delay(3000L)
+            dialogState.value = DialogState.Dismissed
         }
     }
 
